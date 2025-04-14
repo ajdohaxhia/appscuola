c30736aacb8893d566f7a63917be078bbfb21007
ajdohaxhia <ajdohaxhia@gmail.com>
2025-04-14 17:23:00 +0200

    Update build configuration and dependencies for PWA support

--- next.config.js
+++ next.config.js
@@ -6,7 +6,90 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60 // 365 days
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-font-assets',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 7 * 24 * 60 * 60 // 7 days
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-image-assets',
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:js)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-js-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:css|less)$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-style-assets',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        }
+      }
+    },
+    {
+      urlPattern: /\/api\/.*$/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'api-cache',
+        networkTimeoutSeconds: 10,
+        expiration: {
+          maxEntries: 16,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        },
+        matchOptions: {
+          ignoreSearch: true
+        }
+      }
+    },
+    {
+      urlPattern: /.*/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'others',
+        networkTimeoutSeconds: 10,
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        }
+      }
+    }
+  ]
 });
 
 const nextConfig = {
@@ -29,7 +112,7 @@ const nextConfig = {
   },
   // Handle missing document and app errors
   distDir: '.next',
-  webpack(config) {
+  webpack: (config, { isServer }) => {
     // Find the existing rule that handles SVG files
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.('.svg'),
@@ -39,21 +122,32 @@ const nextConfig = {
       // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
-        test: /\.svg\?url$/i, // *.svg?url
-        resourceQuery: /url/, // *.svg?url
+        test: /\.svg\?url$/i,
+        resourceQuery: /url/,
       },
       // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer, // Reuse issuer condition from original rule
-        resourceQuery: { not: [/url/] }, // Exclude if *.svg?url
-        use: [{ loader: '@svgr/webpack', options: { icon: true } }], // Use SVGR loader
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [/url/] },
+        use: [{ loader: '@svgr/webpack', options: { icon: true } }],
       },
     );
 
     // Modify the original rule to ignore *.svg imports handled by SVGR
     if (fileLoaderRule) {
-        fileLoaderRule.exclude = /\.svg$/i;
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    // Add support for workbox-webpack-plugin
+    if (!isServer) {
+      config.plugins.push(
+        new (require('workbox-webpack-plugin').GenerateSW)({
+          swDest: 'sw.js',
+          clientsClaim: true,
+          skipWaiting: true,
+        })
+      );
     }
 
     return config;
